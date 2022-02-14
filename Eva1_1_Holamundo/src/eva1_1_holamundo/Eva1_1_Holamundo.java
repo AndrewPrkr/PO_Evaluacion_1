f01d0e0e5200943bf45203d2a8d7c379f46ad3e7
AndrewPrkr <99374418+AndrewPrkr@users.noreply.github.com>
2022-02-14 14:46:38 -0700

    Add files via upload

---
+++ Eva1_1_Holamundo/src/eva1_1_holamundo/Eva1_1_Holamundo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_holamundo;
+
+/**
+ *
+ * @author saeol
+ */
+public class Eva1_1_Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //  Palabras en azul son palbras reservadas del lenguaje
+        //  Palabras en negro son identificadores => variables,constantes,funciones,objetos, etc. 
+        //  ()Es un arreglo
+        //  {}Establecen bloques de codigo.
+        System.out.println("Hello World");
+        
+    }
+    
+}
