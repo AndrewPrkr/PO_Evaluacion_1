dfe23e9f96efef72200b1a083cbb7265288e5b02
AndrewPrkr <99374418+AndrewPrkr@users.noreply.github.com>
2022-03-10 21:36:08 -0700

    Add files via upload

---
+++ Variables/src/variables/Variables.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author saeol
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int numero = 10; /* para poder declarar una constaante debemos 
+poner final antes de nuestra variable*/
+        
+        
+        
+                
+        System.out.println(numero);
+        
+                
+        
+        // TODO code application logic here
+    }
+    
+}
