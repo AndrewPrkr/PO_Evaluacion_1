3362c4ebfbd5f5660ba593dda3072d9a6219ac68
AndrewPrkr <99374418+AndrewPrkr@users.noreply.github.com>
2022-05-15 22:34:02 -0600

    Add files via upload

---
+++ eva3_11_usar_throw/src/eva3_12_usar_throw/Eva3_12_usar_throw.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_12_usar_throw;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author saeol
+ */
+public class Eva3_12_usar_throw {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        
+        try{
+        System.out.println("Introduce un valor entero positivo; ");
+        int valor = entrada.nextInt();
+        
+        if(valor < 0)
+            //throw sirve para generar una alternativa a las excepciones del sistema.
+            //de esta manera generamos nuestros propios mecanismos de excpecion
+            throw new Exception("El valor" + valor + " no es una edad valida!!");
+             System.out.println("La edad ingresada es : " + valor);
+        
+    }catch(Exception e){
+            e.printStackTrace();
+}
+       
+    }
+}
---
+++ eva3_13_mejorar_captura_edad/src/eva3_13_mejorar_captura_edad/Eva3_13_mejorar_captura_edad.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_13_mejorar_captura_edad;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author saeol
+ */
+public class Eva3_13_mejorar_captura_edad {
+
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner input = new Scanner(System.in);
+         
+         boolean datoErroneo = true;//el usuario siempre se equivoca
+         //para do while se utiliza booleano por terminos de true false
+         
+         do{
+        try{
+            System.out.println("Introduce tu edad en numero entero");
+            int iedad = input.nextInt();
+            System.out.println("Tu edad:" + iedad);
+            
+            
+            
+             if(iedad < 0)
+            //throw sirve para generar una alternativa a las excepciones del sistema.
+            //de esta manera generamos nuestros propios mecanismos de excpecion
+            throw new Exception("El valor" + iedad + " no es una edad valida!!");
+             
+             datoErroneo = false;//el dato es falso el do while ya no se va  a repetir.
+             System.out.println("La edad ingresada es : " + iedad);
+            
+                    
+
+       
+        }
+        
+        catch(InputMismatchException r){
+            System.out.println(input.next() + "no es numero valido");
+        }
+        catch(Exception e){
+            System.out.println("Este no es numero valido");
+            
+        }
+         }while(datoErroneo);//el dato es erroneo se vulve a repetir
+    
+}
+}
